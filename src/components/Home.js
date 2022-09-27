adbede7312d5e120af69cde2fac084875e947f40
Vaman Deshmukh <vamandeshmukh@gmail.com>
2022-09-27 22:47:32 +0530

    early development

--- src/components/Home.js
+++ src/components/Home.js
@@ -7,7 +7,7 @@ const Home = () => {
                 backgroundImage: `url("https://wallpaperaccess.com/full/16692.jpg")`,
                 // backgroundRepeat: `no-repeat`
             }} >
-            <div style={{ minHeight: "100vh" }} className="container d-flex align-items-center">
+            <div style={{ minHeight: "93vh" }} className="container d-flex align-items-center">
                 <p className="display-4 text-light">Deloitte Blog</p>
             </div>
         </div >
